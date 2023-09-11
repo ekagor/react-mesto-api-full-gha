099d4c28910b1797894b155585ae48a72b33a6c6
Ekaterina Gorenkova <gorsuborg@gmail.com>
2023-09-11 13:48:51 +0500

    15.1.14 test

--- frontend/src/components/card/Card.jsx
+++ frontend/src/components/card/Card.jsx
@@ -2,11 +2,11 @@ import { useContext } from "react"
 import CurrentUserContext from "../../contexts/CurrentUserContext.js"
 import ButtonLike from "../buttonLike/ButtonLike.jsx"
 
-export default function Card({ card, onCardClick, openDelete }) {
+export default function Card({ card, onCardClick, openDelete, onCardLike }) {
   const currentUser = useContext(CurrentUserContext)
   return (
     <div className="card__item">
-      {currentUser._id === card.owner._id && <button className="button card__delete-button" type="button" onClick={() => openDelete(card._id)}/>}
+      {currentUser._id === card.owner && <button className="button card__delete-button" type="button" onClick={() => openDelete(card._id)}/>}
       <img className="card__image" 
       src={card.link} 
       alt={`Изображение ${card.name}`} onClick={() => onCardClick({link: card.link, name: card.name})} 
@@ -14,7 +14,8 @@ export default function Card({ card, onCardClick, openDelete }) {
       <div className="card__container">
         <h2 className="text card__subtitle">{card.name}</h2>on
         <div className="card__like-container">
-          <ButtonLike likes={card.likes} myid={currentUser._id} cardid={card._id} />
+          {/* <ButtonLike likes={card.likes} myid={currentUser._id} cardid={card._id} /> */}
+          <ButtonLike myid={currentUser._id} card={card} onCardLike={onCardLike} />
         </div>
       </div>
     </div>
